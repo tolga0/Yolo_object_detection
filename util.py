8cad2085188cc668df301cf04f180d1a9cd956ee
SariTolga <tolgasariuslu@outlook.com.tr>
2020-04-10 08:28:06 +0300

    Add files via upload

---
+++ util.py
@@ -0,0 +1,351 @@
+
+from __future__ import division
+
+import torch 
+import torch.nn as nn
+import torch.nn.functional as F 
+from torch.autograd import Variable
+import numpy as np
+import cv2 
+import matplotlib.pyplot as plt
+from bbox import bbox_iou
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters())
+
+def count_learnable_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+def convert2cpu(matrix):
+    if matrix.is_cuda:
+        return torch.FloatTensor(matrix.size()).copy_(matrix)
+    else:
+        return matrix
+
+def predict_transform(prediction, inp_dim, anchors, num_classes, CUDA = True):
+    batch_size = prediction.size(0)
+    stride =  inp_dim // prediction.size(2)
+    grid_size = inp_dim // stride
+    bbox_attrs = 5 + num_classes
+    num_anchors = len(anchors)
+    
+    anchors = [(a[0]/stride, a[1]/stride) for a in anchors]
+
+
+
+    prediction = prediction.view(batch_size, bbox_attrs*num_anchors, grid_size*grid_size)
+    prediction = prediction.transpose(1,2).contiguous()
+    prediction = prediction.view(batch_size, grid_size*grid_size*num_anchors, bbox_attrs)
+
+
+
+    prediction[:,:,0] = torch.sigmoid(prediction[:,:,0])
+    prediction[:,:,1] = torch.sigmoid(prediction[:,:,1])
+    prediction[:,:,4] = torch.sigmoid(prediction[:,:,4])
+    
+
+    
+
+    grid_len = np.arange(grid_size)
+    a,b = np.meshgrid(grid_len, grid_len)
+    
+    x_offset = torch.FloatTensor(a).view(-1,1)
+    y_offset = torch.FloatTensor(b).view(-1,1)
+    
+    if CUDA:
+        x_offset = x_offset.cuda()
+        y_offset = y_offset.cuda()
+    
+    x_y_offset = torch.cat((x_offset, y_offset), 1).repeat(1,num_anchors).view(-1,2).unsqueeze(0)
+    
+    prediction[:,:,:2] += x_y_offset
+      
+
+    anchors = torch.FloatTensor(anchors)
+    
+    if CUDA:
+        anchors = anchors.cuda()
+    
+    anchors = anchors.repeat(grid_size*grid_size, 1).unsqueeze(0)
+    prediction[:,:,2:4] = torch.exp(prediction[:,:,2:4])*anchors
+
+
+    prediction[:,:,5: 5 + num_classes] = torch.sigmoid((prediction[:,:, 5 : 5 + num_classes]))
+
+    prediction[:,:,:4] *= stride
+   
+    
+    return prediction
+
+def load_classes(namesfile):
+    fp = open(namesfile, "r")
+    names = fp.read().split("\n")[:-1]
+    return names
+
+def get_im_dim(im):
+    im = cv2.imread(im)
+    w,h = im.shape[1], im.shape[0]
+    return w,h
+
+def unique(tensor):
+    tensor_np = tensor.cpu().numpy()
+    unique_np = np.unique(tensor_np)
+    unique_tensor = torch.from_numpy(unique_np)
+    
+    tensor_res = tensor.new(unique_tensor.shape)
+    tensor_res.copy_(unique_tensor)
+    return tensor_res
+
+def write_results(prediction, confidence, num_classes, nms = True, nms_conf = 0.4):
+    conf_mask = (prediction[:,:,4] > confidence).float().unsqueeze(2)
+    prediction = prediction*conf_mask
+    
+
+    try:
+        ind_nz = torch.nonzero(prediction[:,:,4]).transpose(0,1).contiguous()
+    except:
+        return 0
+    
+    
+    box_a = prediction.new(prediction.shape)
+    box_a[:,:,0] = (prediction[:,:,0] - prediction[:,:,2]/2)
+    box_a[:,:,1] = (prediction[:,:,1] - prediction[:,:,3]/2)
+    box_a[:,:,2] = (prediction[:,:,0] + prediction[:,:,2]/2) 
+    box_a[:,:,3] = (prediction[:,:,1] + prediction[:,:,3]/2)
+    prediction[:,:,:4] = box_a[:,:,:4]
+    
+
+    
+    batch_size = prediction.size(0)
+    
+    output = prediction.new(1, prediction.size(2) + 1)
+    write = False
+
+
+    for ind in range(batch_size):
+
+        image_pred = prediction[ind]
+        
+
+        
+
+        max_conf, max_conf_score = torch.max(image_pred[:,5:5+ num_classes], 1)
+        max_conf = max_conf.float().unsqueeze(1)
+        max_conf_score = max_conf_score.float().unsqueeze(1)
+        seq = (image_pred[:,:5], max_conf, max_conf_score)
+        image_pred = torch.cat(seq, 1)
+        
+
+        
+
+        non_zero_ind =  (torch.nonzero(image_pred[:,4]))
+
+        
+        image_pred_ = image_pred[non_zero_ind.squeeze(),:].view(-1,7)
+        
+
+        try:
+            img_classes = unique(image_pred_[:,-1])
+        except:
+             continue
+
+        for cls in img_classes:
+
+            cls_mask = image_pred_*(image_pred_[:,-1] == cls).float().unsqueeze(1)
+            class_mask_ind = torch.nonzero(cls_mask[:,-2]).squeeze()
+            
+
+            image_pred_class = image_pred_[class_mask_ind].view(-1,7)
+
+		
+        
+
+            conf_sort_index = torch.sort(image_pred_class[:,4], descending = True )[1]
+            image_pred_class = image_pred_class[conf_sort_index]
+            idx = image_pred_class.size(0)
+            
+
+            if nms:
+
+                for i in range(idx):
+
+                    try:
+                        ious = bbox_iou(image_pred_class[i].unsqueeze(0), image_pred_class[i+1:])
+                    except ValueError:
+                        break
+        
+                    except IndexError:
+                        break
+                    
+
+                    iou_mask = (ious < nms_conf).float().unsqueeze(1)
+                    image_pred_class[i+1:] *= iou_mask       
+                    
+
+                    non_zero_ind = torch.nonzero(image_pred_class[:,4]).squeeze()
+                    image_pred_class = image_pred_class[non_zero_ind].view(-1,7)
+
+            
+            batch_ind = image_pred_class.new(image_pred_class.size(0), 1).fill_(ind)
+            seq = batch_ind, image_pred_class
+            if not write:
+                output = torch.cat(seq,1)
+                write = True
+            else:
+                out = torch.cat(seq,1)
+                output = torch.cat((output,out))
+    
+    return output
+
+
+
+def predict_transform_half(prediction, inp_dim, anchors, num_classes, CUDA = True):
+    batch_size = prediction.size(0)
+    stride =  inp_dim // prediction.size(2)
+
+    bbox_attrs = 5 + num_classes
+    num_anchors = len(anchors)
+    grid_size = inp_dim // stride
+
+    
+    prediction = prediction.view(batch_size, bbox_attrs*num_anchors, grid_size*grid_size)
+    prediction = prediction.transpose(1,2).contiguous()
+    prediction = prediction.view(batch_size, grid_size*grid_size*num_anchors, bbox_attrs)
+    
+    
+
+    prediction[:,:,0] = torch.sigmoid(prediction[:,:,0])
+    prediction[:,:,1] = torch.sigmoid(prediction[:,:,1])
+    prediction[:,:,4] = torch.sigmoid(prediction[:,:,4])
+
+    
+
+    grid_len = np.arange(grid_size)
+    a,b = np.meshgrid(grid_len, grid_len)
+    
+    x_offset = torch.FloatTensor(a).view(-1,1)
+    y_offset = torch.FloatTensor(b).view(-1,1)
+    
+    if CUDA:
+        x_offset = x_offset.cuda().half()
+        y_offset = y_offset.cuda().half()
+    
+    x_y_offset = torch.cat((x_offset, y_offset), 1).repeat(1,num_anchors).view(-1,2).unsqueeze(0)
+    
+    prediction[:,:,:2] += x_y_offset
+      
+
+    anchors = torch.HalfTensor(anchors)
+    
+    if CUDA:
+        anchors = anchors.cuda()
+    
+    anchors = anchors.repeat(grid_size*grid_size, 1).unsqueeze(0)
+    prediction[:,:,2:4] = torch.exp(prediction[:,:,2:4])*anchors
+
+
+    prediction[:,:,5: 5 + num_classes] = nn.Softmax(-1)(Variable(prediction[:,:, 5 : 5 + num_classes])).data
+
+    prediction[:,:,:4] *= stride
+    
+    
+    return prediction
+
+
+def write_results_half(prediction, confidence, num_classes, nms = True, nms_conf = 0.4):
+    conf_mask = (prediction[:,:,4] > confidence).half().unsqueeze(2)
+    prediction = prediction*conf_mask
+    
+    try:
+        ind_nz = torch.nonzero(prediction[:,:,4]).transpose(0,1).contiguous()
+    except:
+        return 0
+    
+    
+    
+    box_a = prediction.new(prediction.shape)
+    box_a[:,:,0] = (prediction[:,:,0] - prediction[:,:,2]/2)
+    box_a[:,:,1] = (prediction[:,:,1] - prediction[:,:,3]/2)
+    box_a[:,:,2] = (prediction[:,:,0] + prediction[:,:,2]/2) 
+    box_a[:,:,3] = (prediction[:,:,1] + prediction[:,:,3]/2)
+    prediction[:,:,:4] = box_a[:,:,:4]
+    
+    
+    
+    batch_size = prediction.size(0)
+    
+    output = prediction.new(1, prediction.size(2) + 1)
+    write = False
+    
+    for ind in range(batch_size):
+
+        image_pred = prediction[ind]
+
+        
+
+        max_conf, max_conf_score = torch.max(image_pred[:,5:5+ num_classes], 1)
+        max_conf = max_conf.half().unsqueeze(1)
+        max_conf_score = max_conf_score.half().unsqueeze(1)
+        seq = (image_pred[:,:5], max_conf, max_conf_score)
+        image_pred = torch.cat(seq, 1)
+        
+
+        non_zero_ind =  (torch.nonzero(image_pred[:,4]))
+        try:
+            image_pred_ = image_pred[non_zero_ind.squeeze(),:]
+        except:
+            continue
+        
+
+        img_classes = unique(image_pred_[:,-1].long()).half()
+        
+        
+        
+                
+
+        for cls in img_classes:
+
+            cls_mask = image_pred_*(image_pred_[:,-1] == cls).half().unsqueeze(1)
+            class_mask_ind = torch.nonzero(cls_mask[:,-2]).squeeze()
+            
+
+            image_pred_class = image_pred_[class_mask_ind]
+
+
+            conf_sort_index = torch.sort(image_pred_class[:,4], descending = True )[1]
+            image_pred_class = image_pred_class[conf_sort_index]
+            idx = image_pred_class.size(0)
+            
+
+            if nms:
+
+                for i in range(idx):
+
+                    try:
+                        ious = bbox_iou(image_pred_class[i].unsqueeze(0), image_pred_class[i+1:])
+                    except ValueError:
+                        break
+        
+                    except IndexError:
+                        break
+                    
+
+                    iou_mask = (ious < nms_conf).half().unsqueeze(1)
+                    image_pred_class[i+1:] *= iou_mask       
+
+                    non_zero_ind = torch.nonzero(image_pred_class[:,4]).squeeze()
+                    image_pred_class = image_pred_class[non_zero_ind]
+                    
+                    
+
+            batch_ind = image_pred_class.new(image_pred_class.size(0), 1).fill_(ind)
+            seq = batch_ind, image_pred_class
+            
+            if not write:
+                output = torch.cat(seq,1)
+                write = True
+            else:
+                out = torch.cat(seq,1)
+                output = torch.cat((output,out))
+    
+    return output
